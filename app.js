2402248230389eb4d6977e0099123d953c70ebda
shashank <shashankrp2@gmail.com>
2017-09-16 13:57:44 +0530

    Bug fixed

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-function aBigNewFeature(){
-console.log('Writing a lot of code');
-console.log('Halfway done with this new feature now');
-};
-
 function something(){
 console.log('I\m something');
-};
+console.log('Bug fixed!');
+}
 
 console.log('hello world');
 something();
-aBigNewFeature();
